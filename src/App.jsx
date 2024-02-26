f58fa629ed2282eea98a535620967875826552a5
backdfb <wva22lfr@gmail.com>
2024-02-27 01:14:27 +0900

    Banner_Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
+
+import { styled } from 'styled-components'
 import './App.css'
 import Nav from './components/Nav'
 
 function App() {
 
   return (
-    <>
+    <Container>
       <Nav />
-      App
-    </>
+    </Container>
   )
 }
+
+const Container = styled.main`
+  position: absolute;
+  display: block;
+  top: 70px;
+  padding: 0 calc(3.5vw + 5px);
+`
 
 export default App
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,19 @@
 import './Banner.css';
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import axios from '../api/axios';
+import requests from '../api/request';
 
 const Banner = () => {
+
+  useEffect(() => {
+
+  }, [])
+
+  const fetchData = () => {
+    const response = axios.get(requests.fetchNowPlaying);
+  }
+  
   return (
     <div>Banner</div>
   )
