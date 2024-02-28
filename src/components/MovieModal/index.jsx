5fd4941aecc19184227854c901d095f51d468f6a
backdfb <wva22lfr@gmail.com>
2024-02-28 10:58:40 +0900

    moviemodal update

---
+++ src/components/MovieModal/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MovieModal = ({
+    backgrop_path,
+    title, 
+    overview,
+    name,
+    release_date,
+    first_air_date,
+    vote_average,
+    setModalOpen}) => {
+  return (
+    <div>MovieModal</div>
+  )
+}
+
+export default index
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,19 @@
 import { useCallback, useEffect, useState } from 'react';
 import axios from '../api/axios';
 import './Row.css';
+import MovieModal from './MovieModal';
 
 const Row = ({title, id, fetchUrl}) => {
 
-  const [movies, setmovies] = useState([]);  
+  const [movies, setmovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false); 
+  
+  const [movieSelected, setMovieSelected] = useState({});
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  }
 
   const fetchMovieData = useCallback(async () => {
     const response = await axios.get(fetchUrl);
@@ -20,7 +29,13 @@ const Row = ({title, id, fetchUrl}) => {
       <h2>{title}</h2> 
       <div className='slider'>
         <div className="slider__arrow-left">
-          <span className='arrow'>
+          <span className='arrow'
+            onClick={
+                () => {
+                    document.getElementById(id).scrollLeft -= window.innerWidth -80;
+                }
+            }
+          >
             {"<"}
           </span>  
         </div>
@@ -31,16 +46,27 @@ const Row = ({title, id, fetchUrl}) => {
             className='row__poster'
             src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
             />
         ))}
         </div>
         <div className="slider__arrow-right">
-          <span className='arrow'>
+          <span className='arrow'
+          onClick={
+            () => {
+                document.getElementById(id).scrollLeft -= window.innerWidth -80;
+          }}
+          >
             {">"}
           </span>  
         </div>    
 
       </div> 
+
+          {modalOpen ? 
+          <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+          : null}
+
     </div>
   )
 }
