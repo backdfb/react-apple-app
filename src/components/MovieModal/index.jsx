10084367f8b80a18d31144600aed4cebf07c278e
backdfb <wva22lfr@gmail.com>
2024-02-28 23:26:34 +0900

    constant

--- src/components/MovieModal/index.jsx
+++ src/components/MovieModal/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { imageBasePath } from "../../constant"
+import './MovieModal.css'
 
 const MovieModal = ({
     backgrop_path,
@@ -10,7 +11,38 @@ const MovieModal = ({
     vote_average,
     setModalOpen}) => {
   return (
-    <div>MovieModal</div>
+    <div className="presentation" role="presentation">
+        <div className="wrapper-modal">
+            <div className="modal">
+                <span
+                    onClick={() => setModalOpen(false)}
+                    className="modal-close"
+                >
+                    X
+                </span>
+
+                <img 
+                    className="modal__poster-img"
+                    src={`${imageBasePath}${backgrop_path}`}
+                    alt="modal_poaster-img"
+                />
+
+                <div className="modal__content">
+                <p className="modal__details">
+                <span>
+                    100% for you
+                </span>{" "}
+                {release_date ? release_date : first_air_date}
+                </p>
+                <h2 className="modal__title">
+                    {title ? title : name}
+                </h2>
+                <p clasName="modal__overview">평점: {vote_average}</p>
+                <p clasName="modal__overview">{overview}</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
