173ba472157aaa008c9988af06ca628403acb8e0
backdfb <wva22lfr@gmail.com>
2024-02-29 18:47:21 +0900

    update

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,4 +1,16 @@
+import { useLocation } from "react-router-dom"
+
 const SearchPage = () => {
+
+  const location = useLocation(); 
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
+  let query = useQuery();
+  const searchTerm = query.get('q');
+
   return (
     <div>SearchPage</div>
   )
